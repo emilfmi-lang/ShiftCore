69e3fe16e93fc15d3a7dae81127172fd3cba7f0c
emilfmi.lang <emilfmi@code.edu.az>
2026-03-12 22:57:25 +0400

    added AttendanceService methods

--- ShiftCore/Services/AttendanceService.cs
+++ ShiftCore/Services/AttendanceService.cs
@@ -1,5 +1,25 @@
-﻿namespace ShiftCore.Services;
+﻿using ShiftCore.Entity;
+using System.Text.Json;
+
+namespace ShiftCore.Services;
 
 public class AttendanceService
 {
+    private string GetFilePath()
+    {
+        var today = DateTime.Today.ToString("yyyy_MM_dd");
+        return  Path.Combine(Directory.GetCurrentDirectory(), "Data", $"attendance_{today}.json");
+
+    }
+    private List<AttendanceRecord> ReadAttendance()
+    {
+        var filePath = GetFilePath();
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<List<AttendanceRecord>>(json)
+            ?? new List<AttendanceRecord>();
+    }
 }
