200f0e7aee801a91fae952c8d8e5da1f08bb8c18
emilfmi.lang <emilfmi@code.edu.az>
2026-03-13 21:43:39 +0400

    AttendanceService and AttendanceController

--- ShiftCore/Controllers/AttendanceController.cs
+++ ShiftCore/Controllers/AttendanceController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShiftCore.Services;
 
 namespace ShiftCore.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AttendanceController : ControllerBase
+public class AttendanceController(AttendanceService service) : ControllerBase
 {
+    [HttpPost("{workerId}")]
+    public IActionResult RegisterAttendance(Guid workerId)
+    {
+        var result = service.RegisterAttendance(workerId);
+        return Ok(result);
+    }
+    [HttpGet("today")]
+    public IActionResult GetTodayAttendance()
+    {
+        var records = service.GetTodayAttendance();
+        return Ok(records);
+    }
 }
