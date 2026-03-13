f15848e91654d7c34a8cf1827aa23c14a04cf3c1
emilfmi.lang <emilfmi@code.edu.az>
2026-03-13 21:02:33 +0400

    added AttendanceService Methods

--- ShiftCore/Services/AttendanceService.cs
+++ ShiftCore/Services/AttendanceService.cs
@@ -8,7 +8,7 @@ public class AttendanceService
     private string GetFilePath()
     {
         var today = DateTime.Today.ToString("yyyy_MM_dd");
-        return  Path.Combine(Directory.GetCurrentDirectory(), "Data", $"attendance_{today}.json");
+        return Path.Combine(Directory.GetCurrentDirectory(), "Data", $"attendance_{today}.json");
 
     }
     private List<AttendanceRecord> ReadAttendance()
@@ -31,4 +31,41 @@ public class AttendanceService
         });
         File.WriteAllText(path, json);
     }
+    public string RegisterAttendance(Guid workerId)
+    {
+        var records = ReadAttendance();
+        var todayRecord = records.FirstOrDefault(r => r.WorkerId == workerId &&
+                                                 r.Date.Date == DateTime.Today);
+        if (todayRecord == null)
+        {
+            var record = new AttendanceRecord
+            {
+                WorkerId = workerId,
+                Date = DateTime.Today,
+                EntryTime = DateTime.Now
+            };
+            records.Add(record);
+            SaveAttendance(records);
+            return "Entry recorded";
+        }
+        if (todayRecord.ExitTime != null)
+        {
+            return "Exit already recorded";
+        }
+        var diff = DateTime.Now - todayRecord.EntryTime; 
+        if (diff.Value.TotalSeconds < 3)
+        {
+            return "Exit allowed only after 3 hours";
+        }
+        todayRecord.ExitTime = DateTime.Now;
+        SaveAttendance(records);
+        return "Exit recorded";
+    }
+    public List<AttendanceRecord> GetTodayAttendance()
+    {
+        var records = ReadAttendance();
+        return [.. records.Where(r => r.Date.Date == DateTime.Today)];
+
+    }
+
 }
